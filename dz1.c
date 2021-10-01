6da169329af77e65735cfe23c664b0a40fa78ec9
zaplatin <91634981+zaplatin@users.noreply.github.com>
2021-10-01 13:51:32 +0300

    Update dz1.c

--- dz1.c
+++ dz1.c
@@ -1,4 +1,41 @@
-ghbdtn
-hjhj
+#include <iostream>
+#include "stdio.h"
+#include "locale.h"
 
-ghghvhjb
+int my_function(int x)
+{
+	int i, massive[99];
+	for (i = 0; i < 99; i++)
+		{
+			massive[i] = 0;
+		}
+	i = 0;
+	while (x != 0)
+		{
+			if (x % 2 == 0)
+				{
+					massive[i] = 0;
+				}
+			else
+				{
+					massive[i] = 1;
+				}
+			x = x >> 1;
+			i++;
+		}
+	printf("В двоичной системе: ");
+	
+	for (i = i - 1; i >= 0; i--)
+		{ 
+			printf("%d", massive[i]);
+		}	
+	return(0);
+}
+int main(int argc, char* argv[])
+{
+	setlocale(LC_ALL, "RUS");
+	int a;
+	printf("Введите целое десятичное число: ");
+	scanf_s("%d", &a);
+	my_function(a);
+}
