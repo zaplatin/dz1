b04f3a3e23fcf85f7865839b016df18f3ec45392
zaplatin <91634981+zaplatin@users.noreply.github.com>
2021-10-07 22:29:22 +0300

    Update dz1.c

--- dz1.c
+++ dz1.c
@@ -1,37 +1,75 @@
-#include <iostream>
 #include "stdio.h"
 #include "locale.h"
+#include <math.h>
 
 int my_function(int x)
 {
-	int i, massive[99];
-	for (i = 0; i < 99; i++)
+	int i, massive[5] = {};
+	for (i = 0; i < 5; i++)
 		{
 			massive[i] = 0;
 		}
 	i = 0;
-	while (x != 0)
+	if (x == 0)
+	{
+		printf("В двоичной системе: 0");
+	}
+	if (x < 0)
 		{
-			if (x % 2 == 0)
+		int y = abs(x);
+		while (y != 0)
+			{	
+				if (i == 5)
+				{
+					return(1);
+				}
+				if (y % 2 == 0)
 				{
 					massive[i] = 0;
 				}
-			else
+				else
 				{
 					massive[i] = 1;
 				}
-			x = x >> 1;
-			i++;
+				y = y >> 1;
+				i++;
+			}
+			printf("В двоичной системе: -");
+
+			for (i = i - 1; i >= 0; i--)
+			{
+				printf("%d", massive[i]);
+			}
 		}
-	printf("В двоичной системе: ");
-	
-	for (i = i - 1; i >= 0; i--)
-		{ 
-			printf("%d", massive[i]);
-		}	
-	return(0);
+	if (x > 0)
+		{
+			while (x != 0)
+			{	
+				if (i == 5)
+				{
+					return(1);
+				}
+				if (x % 2 == 0)
+				{
+					massive[i] = 0;
+				}
+				else
+				{
+					massive[i] = 1;
+				}
+				x = x >> 1;
+				i++;
+			}
+			printf("В двоичной системе: ");
+
+			for (i = i - 1; i >= 0; i--)
+			{
+				printf("%d", massive[i]);
+			}
+		}
+		return(0);
 }
-int main(int argc, char* argv[])
+int main(void)
 {
 	setlocale(LC_ALL, "RUS");
 	int a;
